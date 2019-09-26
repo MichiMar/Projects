02e1b44dac5d71a9ccf6ee476b855bd07a6336d7
MichiMar <michimar.alexis@gmail.com>
2019-09-26 09:36:55 -0600

    Added the  Js code to Html Code

--- slotmachine.js
+++ slotmachine.js
@@ -1,45 +1,41 @@
-const totalmoney = document.querySelector(".totalMoney");
-const newMoneyInput = document.querySelector("#newMoney");
-totalmoney = parseInt(totalmoney);
-newMoneyInput = parseInt(newMoneyInput);
+    const totalmoney = document.querySelector(".totalMoney");
+    const newMoneyInput = document.querySelector("#newMoney");
+    totalmoney = parseInt(totalmoney);
+    newMoneyInput = parseInt(newMoneyInput);
 
-//const addMoneyBtn = document.querySelector("#addMonney");
-//const retireMoneyBtn = document.querySelector("#retire");
-const numbers = document.querySelector(".numbers");
-//const playBtn = document.querySelector("#playBtn");
+    const numbers = document.querySelector(".numbers");
 
-wallet = () => {
-    // input += money
-    function addMoneyBtn() {
-        totalmoney.innerHTML = totalmoney+newMoneyInput;
-        return addMoneyBtn();
-    }
-    
-    function retireMoneyBtn() {
-        totalmoney.innerHTML = totalmoney-newMoneyInput;
-        return retireMoneyBtn()
-    } 
-}
-
-function game() {
-    if (money >= 10) {
-        const jackpot = document.getElementById("playBtn");
-        jackpot = () => {
-            const bet = totalmoney - 10;
+    wallet = () => {
+        // input += money
+        function addMoney() {
+            totalmoney.innerHTML = totalmoney+newMoneyInput;
+            return addMoneyBtn();
         }
-        //play == random numbers
-        if (playBtn() == true) {
-            
-            numbers.forEach(number => {
-                number=Math.random() * 100;
-            if (number !== number) {
-                return alert("Bad luck, try again!!")
-            } else {
-                totalmoney = totalmoney+bet;
-                return alert('WIIINEEEEER!, YOU REALLY ARE LUCKY TODAY, PLAY AGAIN!!')
+        
+        function retireMoneyBtn() {
+            totalmoney.innerHTML = totalmoney-newMoneyInput;
+            return retireMoney()
+        } 
+    }
+
+    function game() {
+        if (money >= 10) {
+            const jackpot = document.getElementById("playBtn");
+            jackpot = () => {
+                const bet = totalmoney - 10;
             }
-        })
-    
-    }
+            //play == random numbers
+            if (playBtn() == true) {
+                
+                numbers.forEach(number => {
+                    number=Math.random() * 100;
+                    if (number !== number) {
+                        return alert("Bad luck, try again!!")
+                    } else {
+                        totalmoney = totalmoney+bet;
+                        return alert('WIIINEEEEER!, YOU REALLY ARE LUCKY TODAY, PLAY AGAIN!!')
+                    }
+                })
+            }
         }
-}
+    }
